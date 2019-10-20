db278ad275bfb87887d86a622030990f2ff9d598
Masfran <masfran@xcidic.com>
2019-10-20 13:38:09 +0700

    test using json_serializable

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
 class User {
   int id;
   String name;
@@ -11,19 +16,23 @@ class User {
     this.email
   });
 
-  factory User.fromJson(Map<String, dynamic> object) {
-    return User(
-      id: object['id'] ?? 0,
-      name: object['name'] ?? '',
-      username: object['username'] ?? '',
-      email: object['email'] ?? ''
-    );
-  }
+  // equal
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  // factory User.fromJson(Map<String, dynamic> object) {
+  //   return User(
+  //     id: object['id'] ?? 0,
+  //     name: object['name'] ?? '',
+  //     username: object['username'] ?? '',
+  //     email: object['email'] ?? ''
+  //   );
+  // }
 
-  Map<String, dynamic> toJson() => {
-    'id': this.id.toString() ?? '',
-    'name': this.name ?? '',
-    'username': this.username ?? '',
-    'email': this.email ?? ''
-  };
+  //equal
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+  // Map<String, dynamic> toJson() => {
+  //   'id': this.id.toString() ?? '',
+  //   'name': this.name ?? '',
+  //   'username': this.username ?? '',
+  //   'email': this.email ?? ''
+  // };
 }
